38df6ba373817442d4aacd8b02d03fc96500e03f
Jahongir Sobirov <69193276+Jahongir2007@users.noreply.github.com>
2021-04-11 11:21:16 +0500

    Update pynumeral.py

--- bin/pynumeral.py
+++ bin/pynumeral.py
@@ -64,9 +64,11 @@ def format(num, form):
         print(int(num),build)
     elif form == "0a":
         if num >= 1000:
+            val = num / 1000
             build = "k"
-            print(num,build)
+            print(val,build)
         elif num >= 1000000:
+            val = num / 1000000
             build = "m"
             print(num,build)
         else:
@@ -85,59 +87,64 @@ def format(num, form):
         print(reversed(num))
     elif form == "asc":
         print(ascii(num))
-    elif form == "kb":
-        if num == 1024:
+    elif form == "0b":
+        if num <= 1024:
             val = num / 1024
-            build = "KB"
-        elif num > 1024:
-            val = num / 1024
-            build = "KB"
-            print(val,build)
-    elif form == "mb":
-        if num == 1048576:
+            print(val,"KB")
+        elif num >= 1025:
             val = num / 1048576
-            build = "MB"
-            print(val,build)
-        elif num > 1024:
-            val = num / 1024
-            build = "MB"
-            print(val,build)
-    elif form == "gb":
-        if num == 1024:
-            val = num / 1024
-            build = "GB"
-            print(val,build)
-        elif num > 1024:
-            val = num / 1024
-            build = "GB"
-            print(val,build)
-    elif form == "tb":
-        if num == 1024:
-            val = num / 1024
-            build = "TB"
-            print(val,build)
-        elif num > 1024:
-            val = num / 1024
-            build = "TB"
-            print(val,build)
-    elif form == "0k":
-        if num == 1000:
+            print(val,"MB")
+        elif num >= 1048577:
+            val = num / 1073741824
+            print(val,"GB")
+        elif num >= 1073741825:
+            val = num / 1099511627776
+            print(val,"TB")
+    elif form == "hsm":
+        if num < 60:
+            print(num, "seconds")
+        elif num > 60:
+            val = num / 60
+            print(val, "minutes")
+        if num < 3600:
+            val = num / 3600
+            print(val, "hours")
+    elif form == "00:00:00":
+        seconds = "00"
+        minutes = "00"
+        hours = "00"
+        if num < 59:
+            seconds = num
+            print(hours,":",math.ceil(seconds),":",seconds)
+        elif num > 59:
+            minutes = num
+            val = minutes / 60
+            print(hours,":",val,":",seconds)
+    elif form == "0%":
+        if num == 1:
+            num = 100
+            print(num,"%")
+        elif num < 1:
+            val = num * 100
+            print(int(val),"%")
+    elif form == "0a":
+        if num <= 1000000:
             val = num / 1000
-            build = "k"
-            print(int(val),build)
-        elif num > 1000:
-            val =  num / 1000
-            build = "k"
-            print(val,build)
-    elif form == "0m":
-        if num == 1000000:
+            print(val,abb_k)
+        elif num <= 1000000000:
             val = num / 1000000
-            build = "m"
-            print(int(val),build)
-        elif num > 1000000:
-            val =  num / 1000000
-            build = "m"
-            print(val,build)
+            print(num,abb_m)
+        elif num <= 1000000000000:
+            val = num / 1000000000 
+            print(val,abb_b)
+        elif num <= 1000000000000000:
+            val = num / 1000000000000
+            print(val,abb_t)
+    elif form == "0%2":
+        if num % 2 == 0:
+            print("even")
+        else:
+            print("odd")
 def add(num, set):
     print(num + set)
 def sub(num, set):
@@ -150,3 +157,40 @@ def dif(num, set):
     print(num - set)
 def val(value):
     print(value)
+def clone(name, set):
+    print("Name clone: ", name)
+    print("Value clone: ", set)
+def value(get):
+    print(get)
+def zeroform(code, num):
+        print("number:",code)
+def makelang(lang,abb_k,abb_m,abb_b,abb_t,curr,num):
+    if num <= 1000000:
+        val = num / 1000
+        print(val,abb_k)
+    elif num <= 1000000000:
+        val = num / 1000000
+        print(num,abb_m)
+    elif num <= 1000000000000:
+        val = num / 1000000000 
+        print(val,abb_b)
+    elif num <= 1000000000000000:
+        val = num / 1000000000000
+        print(val,abb_t)
+    print(curr,num)
+def unform(num,type):
+    if type == "0.0":
+        print(int(num))
+    elif type == "||":
+        print(num)
+    elif type == "0.!0":
+        print(float(num))
+    elif type == "0k":
+        val = num * 1000
+        print(val)
+    elif type == "0m":
+        val = num * 1000000
+        print(val)
+    elif type == "0%":
+        val = num / 100
+        print(val)
